6aa678609ede3c030e77c0c368b9f09be2d749a4
Sirojiddin Yunusov <sirojiddinyunusov572@gmail.com>
2025-04-11 18:14:07 +0500

    фы

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,19 +1,28 @@
-import { Route, Routes } from 'react-router-dom'
-import Layouts from '../Layouts/layouts'
-import Coomponent from '../components/coomponent/coomponent'
+import React from 'react'
+import { Routes, Route, useLocation } from 'react-router-dom'
+import Navbar from '../components/Navbar/Navbar'
+import Navbar2 from '../components/Navbar/Navbar2'
+import Footer from '../components/Footer/Footer'
 import Home from '../components/homeComponent/Home'
+import Coomponent from '../components/coomponent/coomponent'
 import Menu from '../components/menu/menu'
 
-function Router() {
+function Layouts() {
+    const { pathname } = useLocation()
+
     return (
-        <Routes>
-            <Route path="/" element={<Layouts />}>
-                <Route index element={<Home />} />
-                <Route path="component" element={<Coomponent />} />
-                <Route path="menu" element={<Menu />} />
-            </Route>
-        </Routes>
+        <div>
+            {pathname === '/' ? <Navbar2/> : <Navbar/>}
+
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/component" element={<Coomponent />} />
+                <Route path="/menu" element={<Menu />} />
+            </Routes>
+
+            <Footer/>
+        </div>
     )
 }
 
-export default Router
+export default Layouts
