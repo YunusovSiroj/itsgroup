f4e0bf8d7982343951300d435337987a79d49298
Sirojiddin Yunusov <sirojiddinyunusov572@gmail.com>
2025-05-05 17:42:45 +0500

    фыф

--- src/components/Navbar/Navbar2.jsx
+++ src/components/Navbar/Navbar2.jsx
@@ -1,12 +1,12 @@
+import "./Navbar2.scss";
 import React, { useEffect, useState } from "react";
-import img2 from "../../assets/img/Mask group1.webp";
 import { MdMenu } from "react-icons/md";
 import { IoEarth } from "react-icons/io5";
-import "./Navbar.scss";
+import img1 from "../../assets/img/Mask group.webp";
 import Menu from "../menu/menu";
 import axios from "axios";
 
-function Navbar() {
+export default function Navbar2() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -23,18 +23,12 @@ function Navbar() {
         setData(res.data);
       } catch (err) {
         if (err.response) {
-          // Сервер ответил с кодом состояния, отличным от 2xx
-          console.error("Ошибка ответа сервера:", err.response);
           setError(
             `Ошибка сервера: ${err.response.status} - ${err.response.statusText}`
           );
         } else if (err.request) {
-          // Запрос был сделан, но ответа не получено
-          console.error("Ошибка запроса:", err.request);
           setError("Ошибка сети: не получен ответ от сервера.");
         } else {
-          // Произошла ошибка при настройке запроса
-          console.error("Ошибка настройки запроса:", err.message);
           setError(`Ошибка: ${err.message}`);
         }
       } finally {
@@ -46,33 +40,29 @@ function Navbar() {
   }, []);
 
   return (
-    <div className="navbar-container">
-      <div className="navbar2">
-        <img src={img2} alt="Logo" />
-
-        <div className="navbar3">
-          <div className="dropdown">
-            <IoEarth />
-            <button className="dropdown-button">Eng ▼</button>
-            <div className="dropdown-menu">
-              <a href="#">Eng</a>
-              <a href="#">Рус</a>
-              <a href="#">Uz</a>
+    <div>
+      <div className="navbar1">
+        <div className="navbar2">
+          <img src={img1} alt="Logo" />
+          <div className="navbar3">
+            <div className="dropdown">
+              <IoEarth />
+              <button className="dropdown-button">Eng ▼</button>
+              <div className="dropdown-menu">
+                <a href="#">Eng</a>
+                <a href="#">Рус</a>
+                <a href="#">Uz</a>
+              </div>
             </div>
+            <button onClick={toggleMenu} className="menu-toggle-button">
+              <MdMenu size={28} />
+            </button>
           </div>
-
-          <button onClick={toggleMenu} className="menu-toggle-button">
-            <MdMenu />
-          </button>
         </div>
       </div>
 
       {isMenuOpen && (
-        <Menu
-          data={data}
-          isMenuOpen={isMenuOpen}
-          setIsMenuOpen={setIsMenuOpen}
-        />
+        <Menu data={data} isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
       )}
 
       <div className="translation-list">
@@ -91,5 +81,3 @@ function Navbar() {
     </div>
   );
 }
-
-export default Navbar;
